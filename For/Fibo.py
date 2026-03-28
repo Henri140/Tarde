30e9ea2f15155a986c41f20e1d2373db695d65c6
Henri140 <henrique.ssalles140@gmail.com>
2026-03-28 13:32:59 -0300

    Fibo=temp

--- For/Fibo.py
+++ For/Fibo.py
@@ -1,19 +1,12 @@
-n=0
-for i in range(0,100,n+1):
-    if i==0:
-        print(i)
-        i+=1
-        n=i
-    elif i==1:
-     print(i)
-     print(i)
-     i+=1
-     n = i
-    else:       
-        print(n)
-        n+=i-1
-        if n>=8:
-           print(n)
-           n+=i+1
+a = 0
+b = 1
+while a < 1000:
+    print(a)
+    temp = b
+    b = a + b
+    a = temp
+
+
+
            
                       
